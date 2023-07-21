387249fa239bc6147bf5ab6fb3ce8fca1d455a6c
Alberto Iong <albert0i@hotmail.com>
2023-07-21 11:32:13 +0800

    getStaitcProps

--- src/pages/posts.jsx
+++ src/pages/posts.jsx
@@ -6,8 +6,8 @@ function Posts({ posts }) {
   return <PostList posts={posts} />
 }
 
-export async function getServerSideProps() {
-  console.log('===> getServerSideProps')
+export async function getStaticProps() {
+  console.log('===> getStaticProps')
 
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   const posts = await response.json();
